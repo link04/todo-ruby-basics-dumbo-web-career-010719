7614e628e76abacaddf97c698063add991d98c56
Maximo Bautista <max.bautista97@gmail.com>
2018-12-16 13:29:13 -0400

    Lab Completed

--- lib/ruby_basics.rb
+++ lib/ruby_basics.rb
@@ -1,27 +1,28 @@
 def division(num1, num2)
- 
+ result = num1/num2
+ result
 end
 
 def assign_variable(value)
-  
+  name = value
 end
 
-def argue
-  
+def argue(argument)
+  argument
 end
 
-def greeting
+def greeting(greeting, name)
 
 end
 
 def return_a_value
-
+  phrase = "Nice"
 end
 
 def last_evaluated_value
-
+  phrase = "expert"
 end
 
-def pizza_party
-
+def pizza_party(ingredient = "cheese")
+  ingredient
 end
